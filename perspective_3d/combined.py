f29c139b1e22a844f9941bba4daf93dd365f5187
Gaurav Juvekar <gauravjuvekar@gmail.com>
2016-10-17 23:54:04 +0530

    Add comments, and better checking whether a point or an iterable of points is given

--- perspective_3d/combined.py
+++ perspective_3d/combined.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Plots a line using Bresenham's algorithm (integer arithmetic only)
+Shows a rotating cube in parallel and perspective projections
 """
 import pygame
 import argparse
@@ -8,7 +8,6 @@ import math
 from collections import namedtuple
 import time
 import math
-import pprint
 
 # A simple point class
 Point = namedtuple('Point', ['x', 'y', 'z'])
@@ -21,9 +20,10 @@ class UnitVector(object):
         self.y = y / det
         self.z = z / det
 
+
 def sign(number):
     """
-    Returns signum(`number`)
+    Return signum(`number`)
         -1 if `number` < 0,
         +1 if `number` > 0,
         0 if `number` == 0)
@@ -38,34 +38,54 @@ def plot(point, color=(255, 255, 255)):
     """
     surface.set_at((point.x, point.y), color)
 
+
 def irange(start, end, abs_delta=1):
     """
-    Returns an inclusive range() generator in steps of `abs_delta`
+    Return an inclusive 'range()' generator in steps of `abs_delta`
     """
     diff = end - start
     return range(start, end + sign(diff), sign(diff) * abs_delta)
 
+
 def translate(points, shift_x, shift_y, shift_z):
-    try:
-        iter_points = iter(points)
-    except TypeError:
+    """
+    Translate either a single 'Point()' or an iterable of 'Point()'s
+    Return:
+        iterable of translated 'Point()'s
+    """
+    if isinstance(points, Point):
         iter_points = iter((points,))
+    else:
+        iter_points = iter(points)
     for point in iter_points:
         yield Point(point.x + shift_x, point.y + shift_y, point.z + shift_z)
 
+
 def scale(points, scale_x, scale_y, scale_z):
-    try:
-        iter_points = iter(points)
-    except TypeError:
+    """
+    Scale either a single 'Point()' or an iterable of 'Point()'s
+    Return:
+        iterable of scaled 'Point()'s
+    """
+    if isinstance(points, Point):
         iter_points = iter((points,))
+    else:
+        iter_points = iter(points)
     for point in iter_points:
         yield Point(point.x * scale_x, point.y * scale_y, point.z * scale_z)
 
+
 def rotate(points, angle, axis):
-    try:
-        iter_points = iter(points)
-    except TypeError:
+    """
+    Rotates either a single 'Point()' or an iterable of 'Point()'s about `axis`
+    by `angle` (in radians).
+    Return:
+        iterable of rotated 'Point()'s
+    """
+    if isinstance(points, Point):
         iter_points = iter((points,))
+    else:
+        iter_points = iter(points)
     l = axis.x
     m = axis.y
     n = axis.z
@@ -89,38 +109,83 @@ def rotate(points, angle, axis):
                      matrix[2][1] * point.y +
                      matrix[2][2] * point.z))
 
+
 def rotate_faces(faces, angle, axis):
+    """
+    Convenience wrapper to rotate faces defined as an iterable of 'Point()'s
+    about `axis` by `angle` radians.
+    Returns:
+        iterable of iterables of rotated face 'Point()'s
+    """
     return (rotate(face, angle, axis) for face in faces)
 
+
 def intify(points):
-    try:
-        iter_points = iter(points)
-    except TypeError:
+    """
+    Round `points` (which can be a single 'Point()' or an iterable of
+    'Point()'s) coordinates to integers.
+    Return:
+        iterable of 'Point()'s
+    """
+    if isinstance(points, Point):
         iter_points = iter((points,))
+    else:
+        iter_points = iter(points)
     for point in iter_points:
-        yield Point(int(round(point.x)), int(round(point.y)))
+        yield Point(int(round(point.x)),
+                    int(round(point.y)),
+                    int(round(point.z)))
+
 
 def animate(poly_shape, vector, rotation_range):
+    """
+    Animate `poly_shape` (iterable of faces which are themselves iterables of
+    'Point()'s) by rotating it about `vector` in 1 degree steps from 0 to
+    `rotation_range` degrees.
+    Return:
+        iterable of each rotated shape
+    """
     for angle in rotation_range:
         faces = [[point for point in face] for face in
                  rotate_faces(poly_shape, math.radians(angle), vector)]
         yield list(faces)
 
+
 def perspective_project(point, cam, screen_z):
+    """
+    Returns the perspective projection of `point` onto a screen parallel to the
+    XY plane with Z coordinate `screen_z`, and center of projection as `cam`.
+    Return:
+        'Point()' on the screen
+    """
     x = cam.x + ((screen_z - cam.z) * (cam.x - point.x) / (point.z - cam.z))
     y = cam.y + ((screen_z - cam.z) * (cam.y - point.y) / (point.z - cam.z))
-    # return Point(x, y, 0)
-    return (x, y)
+    return Point(x, y, screen_z)
 
-def parallel_project(point):
-    # return Point(point.x, point.y)
+
+def parallel_project(point, screen_z=0):
+    """
+    Returns the parallel projection of `point` onto a screen parallel to the
+    XY plane.
+    Return:
+        'Point()' on the screen
+    """
+    return Point(point.x, point.y, screen_z)
+
+
+def flatten(point):
+    """
+    Convert a 3D 'Point()' `point` into a tuple of screen coordinates
+    """
+    point = list(intify(point))[0]
     return (point.x, point.y)
+
 
 def main(args):
     cam = Point(args.cam_x, args.cam_y, args.cam_z)
     vector = UnitVector(args.VECTOR_X, args.VECTOR_Y, args.VECTOR_Z)
 
-    # Define a cube
+    # Define a cube as a list of faces with each face being a list of 'Point()'s
     cube = []
     CUBE_SIDE = 200
     face = [Point(*_) for _ in (
@@ -156,8 +221,10 @@ def main(args):
     screen = pygame.display.set_mode((args.window_size, args.window_size))
     surface = pygame.display.get_surface()
 
+    # Get incrementally rotated cubes
     for cube in animate(cube, vector, range(args.angle + 1)):
-        # Shift it at the center of the screen
+        # Shift it at the center of the screen, and make its Z coordinates
+        # negative.
         cube = [list(translate(face,
                                args.window_size // 2,
                                args.window_size // 2,
@@ -165,25 +232,35 @@ def main(args):
         perspectives = [[perspective_project(point, cam, args.screen_z)
                          for point in face]
                         for face in cube]
-        parallels= [[parallel_project(point) for point in face]
-                    for face in cube]
+        parallels = [[parallel_project(point) for point in face]
+                     for face in cube]
 
+        # Shift projections to appear side by side
+        perspectives = [list(translate(face, +CUBE_SIDE * 1, 0, 0))
+                        for face in perspectives]
+        parallels = [list(translate(face, -CUBE_SIDE * 1, 0, 0))
+                     for face in parallels]
+
+        # Convert to tuples as pygame.draw takes only 2d tuples
+        parallels = [[flatten(point) for point in face] for face in parallels]
+        perspectives = [[flatten(point) for point in face]
+                        for face in perspectives]
         for polygon in perspectives:
             pygame.draw.polygon(surface, (255, 255, 255), polygon, 1)
         for polygon in parallels:
             pygame.draw.polygon(surface, (255, 0, 255), polygon, 1)
 
         pygame.display.update()
-        time.sleep(0.02)
+        time.sleep(0.01)
         surface.fill((0,0,0))
 
     # Wait till window quit
-    time.sleep(2)
-    # while pygame.event.wait().type != pygame.QUIT:
-        # pass
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
 
 
 if __name__ == '__main__':
+    # Get command line arguments
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("VECTOR_X",
@@ -198,15 +275,15 @@ if __name__ == '__main__':
     parser.add_argument("--cam_x",
                         type=int,
                         help="X coordinate of camera",
-                        default=250)
+                        default=500)
     parser.add_argument("--cam_y",
                         type=int,
                         help="Y coordinate of camera",
-                        default=250)
+                        default=500)
     parser.add_argument("--cam_z",
                         type=int,
                         help="Z coordinate of camera",
-                        default=300)
+                        default=500)
     parser.add_argument("--screen-z",
                         type=int,
                         help="Z coordinate of screen",
@@ -217,7 +294,7 @@ if __name__ == '__main__':
                         default=360)
     parser.add_argument("--window-size", "-w",
                         type=int,
-                        default=500,
+                        default=1000,
                         help="Window size in pixels (equal width an height)")
     args = parser.parse_args()
     main(args)
